fde433e4beb4b41f7412c036f94340d98dc39167
Glen Carpenter <glen@glencarpenter.io>
2019-02-22 15:29:06 -0700

    Add files via upload
    Added grid option

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,27 @@
 var input = $("#color-field");
 let paintColor = "#000";
 
+$(document).ready(function() {
+    
+//on click change square background color to the input value
+	$(".square").on("click", function(){
+		if(!$(this).hasClass("painted")){
+			$(this).css('background-color', paintColor);
+			$(this).addClass("painted");
+		} else {
+			$(this).css('background-color', "#fff");
+			$(this).removeClass("painted");
+		}
+	})
 
-$(".square").on("click", function(){
-	if(!$(this).hasClass("painted")){
-		$(this).css('background-color', paintColor);
-		$(this).addClass("painted");
-	} else {
-		$(this).css('background-color', "#fff");
-		$(this).removeClass("painted");
-	}
-})
+	$('input[type=checkbox]').on('change',function(e) {
+		$('.square').toggleClass('grid');
+	});
+});
 
 function selectColor(color) {
-	paintColor = color;
-}
+		paintColor = color;
+	}
 
 document.getElementById("btnSave").addEventListener("click", function() {
 
